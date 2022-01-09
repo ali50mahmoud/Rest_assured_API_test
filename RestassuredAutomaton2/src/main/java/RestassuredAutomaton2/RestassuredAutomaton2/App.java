0c42155aeb3a278a0583f6e75497e9fb05e679e5
Ali Mahmoud <alimahmoud2245076@gmail.com>
2022-01-09 20:57:58 +0200

    initial commit

---
+++ RestassuredAutomaton2/src/main/java/RestassuredAutomaton2/RestassuredAutomaton2/App.java
@@ -0,0 +1,13 @@
+package RestassuredAutomaton2.RestassuredAutomaton2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
